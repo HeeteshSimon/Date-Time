cb4e8aaa1065e5302baca8aecde3a70edd1491fc
HeeteshSimon <heeteshsimon@gmail.com>
2020-04-15 23:50:19 +0530

    Added Comments to the code

--- Date-Time.py
+++ Date-Time.py
@@ -1,9 +1,19 @@
+#Import the DateTime Module
 import datetime
+#Wrote the code to check the working of module-By Checking the Current date
 today=datetime.date.today()
+#Print It
 print(today)
+#Checked the B'day Thing-By specifying the DOB
 birthday=datetime.date(1996,5,19)
+#Print It
 print(birthday)
+#Checked The no. of days I'm alive-By finding the difference b/w today and DOB,
+#Alse, Make sure that it's only returning no. of days not the complete object-By including the "('').days"
 alive_days=(today-birthday).days
+#Print It
 print(alive_days)
+#Added 10 days to the current date
 tdelta=datetime.timedelta(days=10)
+#Print It
 print(today+tdelta)
